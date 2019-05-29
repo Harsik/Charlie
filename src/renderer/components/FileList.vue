b8ef6a285f7dc79f48a26a9e63a90c8c72524714
aznm12 <aznm12@archivsoft.com>
2019-05-29 15:36:44 +0900

    dirty but useful

--- src/renderer/components/FileList.vue
+++ src/renderer/components/FileList.vue
@@ -1,72 +1,76 @@
 <template>
   <v-layout align-start justify-center row wrap>
     <v-flex xs12 sm12 md12>
-      <v-toolbar flat color="white">
+      <v-toolbar flat color='white'>
         <v-toolbar-title>FileList</v-toolbar-title>
       </v-toolbar>
-      <v-card class="pa-3">
+      <v-card class='pa-3'>
         <v-card-title>
           <v-spacer></v-spacer>
           <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
+            v-model='search'
+            append-icon='search'
+            label='Search'
             single-line
             hide-details
           ></v-text-field>
         </v-card-title>
         <v-data-table
-          v-model="selected"
-          :headers="headers"
-          :items="files"
-          :search="search"
-          item-key="name"
+          v-model='selected'
+          :headers='headers'
+          :items='files'
+          :search='search'
+          item-key='name'
           select-all
-          class="elevation-1"
+          class='elevation-1'
         >
           <!-- hide-actions
-          :pagination.sync="pagination"-->
-          <v-progress-linear v-slot:progress color="blue" indeterminate></v-progress-linear>
-          <template v-slot:items="props">
+          :pagination.sync='pagination'-->
+          <v-progress-linear v-slot:progress color='blue' indeterminate></v-progress-linear>
+          <template v-slot:items='props'>
             <td>
-              <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
+              <v-checkbox v-model='props.selected' primary hide-details></v-checkbox>
             </td>
             <td>
-              <a v-bind:href="props.item.downloadUri">{{ props.item.name }}</a>
+              <a v-bind:href='props.item.downloadUri'>{{ props.item.name }}</a>
             </td>
             <td>{{ props.item.type }}</td>
             <td>{{ props.item.size }}</td>
             <td>{{ props.item.updatedAt }}</td>
-            <td class="align-center justify-start layout px-0">
-              <v-icon small @click="deleteFile(props.item.name)">delete</v-icon>
+            <td class='align-center justify-start layout px-0'>
+              <v-icon small @click='deleteFile(props.item.name)'>delete</v-icon>
             </td>
           </template>
           <!-- <template v-slot:no-results>
-        <v-alert :value="true" color="error" icon="warning">
-          Your search for "{{ search }}" found no results.
+        <v-alert :value='true' color='error' icon='warning'>
+          Your search for '{{ search }}' found no results.
         </v-alert>
           </template>-->
         </v-data-table>
         <!-- <div class='text-xs-center pt-2'>
           <v-pagination v-model='pagination.page' :length='pages'></v-pagination>
         </div>-->
-        <v-btn raised class="primary" @click="onPickFile">Upload</v-btn>
+        <v-btn raised class='primary' @click='onPickFile'>Upload</v-btn>
         <input
-          type="file"
-          style="display: none"
-          ref="fileInput"
-          accept="*"
-          @change="onFilePicked"
+          type='file'
+          style='display: none'
+          ref='fileInput'
+          accept='*'
+          @change='onFilePicked'
           multiple
           required
         >
-        <v-btn raised class="primary" @click="downloadFiles">Download</v-btn>
+        <v-btn raised class='primary' @click='downloadFile'>Download</v-btn>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+// import http from 'http'
+// import fs from 'fs'
+// import path from 'path'
+
 export default {
   name: 'FileList',
   data: () => ({
@@ -91,8 +95,8 @@ export default {
       },
       { text: 'Type', value: 'type' },
       { text: 'Size', value: 'size' },
-      {text: 'UpdatedAt', value: 'updatedAt'},
-      {text: '', value: ''}
+      { text: 'UpdatedAt', value: 'updatedAt' },
+      { text: '', value: '' }
     ]
   }),
   // beforeCreate () {
@@ -104,25 +108,14 @@ export default {
   mounted () {
     this.loadFiles()
   },
-  // watch: {
-  //   pages () {
-  //     // console.log(this.pagination)
-  //     if (
-  //       this.pagination.rowsPerPage == null ||
-  //       this.pagination.totalItems == null
-  //     ) { return 0 }
-  //     return Math.ceil(
-  //       this.pagination.totalItems / this.pagination.rowsPerPage
-  //     )
-  //   }
-  // },
   computed: {
     pages () {
-      // console.log(this.pagination)
       if (
         this.pagination.rowsPerPage == null ||
         this.pagination.totalItems == null
-      ) { return 0 }
+      ) {
+        return 0
+      }
       return Math.ceil(
         this.pagination.totalItems / this.pagination.rowsPerPage
       )
@@ -130,41 +123,121 @@ export default {
   },
   methods: {
     downloadFiles () {
-      let fileNames = []
-      // let fileNames = { 'fileNames': {} }
-      // this.selected.forEach(function (value, key) {
-      //   fileNames.fileNames.push(value.name)
-      // })
       this.selected.forEach(function (value, key) {
-        fileNames.push('fileNames', value.name)
+        console.log(value)
+        console.log(value.name)
+        this.downloadFile(value.name)
       })
+    },
+    downloadFile () {
+      let fileName = this.selected[0].name
       const headers = new Headers({
-        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/json'
       })
 
       if (localStorage.accessToken) {
         headers.append('Authorization', 'Bearer ' + localStorage.accessToken)
       }
-      fetch('http://localhost:8080/api/file/downloadMultipleFiles', {
-        method: 'POST',
-        headers: headers,
-        body: fileNames
-      })
-        .then((
-          response // response.ok 값을 남기기 위해 respoense.json().then으로 다시 출력
-        ) =>
-          response.json().then(json => {
-            if (!response.ok) {
-              return Promise.reject(json)
-            }
-            console.log(json)
-            this.loadFiles()
-          })
-        )
-        .catch(error => {
-          console.log(error)
-          this.errorAlarm()
+      // const file = fs.createWriteStream(fileName)
+      // console.log(file)
+      // const request = http.get(
+      //   'http://localhost:8080/api/file/downloadFile/' + fileName,
+      //   function (response) {
+      //     response.pipe(file)
+      //     console.log(response)
+      //   }
+      // )
+      // console.log(request)
+      fetch('http://localhost:8080/api/file/downloadFile/' + fileName, {
+        method: 'GET',
+        headers: headers
+      }).then(response => {
+      const reader = response.body.getReader()
+        return new ReadableStream({
+      start(controller) {
+        return pump()
+      function pump() {
+        return reader.read().then(({ done, value }) => {
+          // When no more data needs to be consumed, close the stream
+          if (done) {
+              controller.close()
+              return
+          }
+          // Enqueue the next data chunk into our target stream
+          controller.enqueue(value)
+          return pump()
         })
+      }
+    }  
+  })
+})
+.then(stream => new Response(stream))
+.then(response => response.blob())
+.then(blob => URL.createObjectURL(blob))
+.then(url => console.log(image.src = url))
+.catch(err => console.error(err))
+console.log(pump())
+        // .then(response => {
+        //   let appPath = path.join(this.$electron.remote.app.getPath('appData'), 'elecapp', fileName)
+        //   const reader = response.body.getReader()
+        //   const stream = new ReadableStream({
+        //     start (controller) {
+        //       // The following function handles each data chunk
+        //       function push () {
+        //         // 'done' is a Boolean and value a 'Uint8Array'
+        //         reader.read().then(({ done, value }) => {
+        //           // Is there no more data to read?
+        //           if (done) {
+        //             // Tell the browser that we have finished sending data
+        //             controller.close()
+        //             return
+        //           }
+        //           fs.writeFile(appPath, value, (err) => {
+        //             if (err) return null
+        //           })
+        //           fs.createWriteStream(fileName)
+
+        //           // Get the data and send it to the browser via the controller
+        //           controller.enqueue(value)
+        //           push()
+        //         })
+        //       }
+
+        //       push()
+        //     }
+        //   })
+        //   return new Response(stream, {
+        //     headers: { 'Content-Type': 'text/html' }
+        //   })
+        //   // let appPath = path.join(this.$electron.remote.app.getPath('appData'), 'elecapp', fileName)
+        //   // console.log(response.body)
+        //   // fs.writeFile(appPath, response.body, (err) => {
+        //   //   if (err) return null
+        //   // })
+        //   // fs.createWriteStream(fileName)
+
+        //   // const responseToReadable = response => {
+        //   // const reader = response.body.getReader()
+        //   // const rs = new Readable()
+        //   // rs._read = async () => {
+        //   //   const result = await reader.read()
+        //   //   if (!result.done) {
+        //   //     rs.push(Buffer.from(result.value))
+        //   //   } else {
+        //   //     rs.push(null)
+        //   //     return null
+        //   //   }
+        //   // }
+
+        //   // return rs
+        //   // }
+        //   // console.log(response)
+        //   // this.loadFiles()
+        // })
+        // .catch(error => {
+        //   console.log(error)
+        //   this.errorAlarm()
+        // })
     },
     deleteFile (fileName) {
       const headers = new Headers({
@@ -187,7 +260,7 @@ export default {
             this.loadFiles()
           })
         )
-        .catch((error) => {
+        .catch(error => {
           console.log(error)
           this.errorAlarm()
         })
@@ -220,6 +293,10 @@ export default {
       for (var index = 0; index < files.length; index++) {
         formData.append('files', files[index])
       }
+      // files.forEach(function (value, key) {  //not function
+      //   formData.append('files', files[key])
+      //   formData.append('files', value)
+      // })
       const headers = new Headers({
         // 'Content-Type': 'multipart/form-data'
       })
